31b9db6f1746e4a2224cef7ae9be057095c5625f
Tech-A45 <tech-a45@Tech-A45xs-MacBook-Air.local>
2018-11-27 17:14:12 +0000

    added the feature where user enters name

---
+++ main.js
@@ -0,0 +1,17 @@
+var name = prompt("What is your name?");
+
+var nameAlert = alert("Hi " + name + ", lets try and survive. Here's how your day is going to start...");
+
+var scenarioOneAlert = alert("You really aren't awake yet. You never are until at least your second cup of coffee, and this is only your first. You're having a hard time getting your eyes to focus.")
+
+var decisionOne = prompt("What do you do? A - Get up and wash your face. B - Go back to sleep. C - Have your second cup of coffee");
+
+// if ("Get up and wash your face") {
+//   alert("You have entered the bathroom");
+// } else if ("Go back to sleep") {
+
+//   alert("You are asleep");
+
+// } else ("Have your second cup of coffee"); {
+//   alert("You have entered your kitchen");
+// }
